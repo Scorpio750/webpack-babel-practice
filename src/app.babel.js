90c80f81e3ec288cf31d0db4d4f7a9fca492ddb1
Patrick Wu <scorpio7500@gmail.com>
2016-09-22 06:04:38 -0400

    starting over from basics

---
+++ src/app.babel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _cats = require('./cats.js');
+
+var _cats2 = _interopRequireDefault(_cats);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_cats2.default);
